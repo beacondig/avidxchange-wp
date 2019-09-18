3df6f4dc1f8c8996717a16053101f637c08fa0b7
Bagus <25834188+contactjavas@users.noreply.github.com>
2019-09-18 13:40:05 +0700

    responsiveness

--- partials/sections/section-request-demo.php
+++ partials/sections/section-request-demo.php
@@ -12,7 +12,7 @@ defined( 'ABSPATH' ) || die( "Can't access directly" );
 	<div class="container">
 
 		<div class="row">
-			<div class="col-xs-12 col-sm-6">
+			<div class="col-xs-12 col-md-6">
 				<?php
 				$image = get_field( 'request_demo_image' );
 				$image = isset( $image['sizes'] ) && isset( $image['sizes']['medium_large'] ) ? $image['sizes']['medium_large'] : '';
@@ -22,7 +22,7 @@ defined( 'ABSPATH' ) || die( "Can't access directly" );
 					<img alt="Illustration image" src="<?php echo esc_url( $image ); ?>">
 				<?php endif; ?>
 			</div>
-			<div class="col-xs-12 col-sm-6">
+			<div class="col-xs-12 col-md-6">
 				<div class="form-wrapper fadein">
 					<h2 class="section-title is-white has-smaller-gap"><?php the_field( 'request_demo_title' ); ?></h2>
 					<?php echo do_shortcode( '[gravityform id=38 title=false description=false ajax=true tabindex=49]' ); ?>
