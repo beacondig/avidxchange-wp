a9305b0908c807e2f5e4fff23879819dff4b4059
Michael McCoy <mmccoy@atmosol.com>
2019-09-18 07:55:38 -0400

    Moving Menu functions into global functions file

--- js/home-functions.js
+++ js/home-functions.js
@@ -8,25 +8,6 @@ jQuery(function() {
 		// Opening FadeIn
 		window.onload  = setTimeout(displayForm, 1500);
 		
-		// Sticky menu functionality
-		function moveScroller() {
-			var move = function() {
-				var st = jQuery(window).scrollTop();
-				var ot = jQuery('.home-opening').height();
-				var s = jQuery('.header-scroll');
-				if(st >= ot) {
-					s.addClass('active');
-				} else {
-					if(st < ot) {
-						s.removeClass('active');
-					}
-				}
-			};
-			jQuery(window).scroll(move);
-			move();
-		}
-		moveScroller();
-		
 		// Home Details click
 		jQuery('.home-details .selections ul li').on('click', function() {
 			var imgSrc = jQuery(this).data('img');
@@ -100,27 +81,4 @@ jQuery(function() {
 		});
 		
 		//jQuery('.home-testimonials ul li:first-child').addClass('active');
-		
-		//Mobile Menu Functionailty
-		jQuery('.navicon').on('click', function() {
-			jQuery('.mobile-nav .m-nav-container').addClass('open');
-		});
-		
-		jQuery('.mobile-close').on('click', function() {
-			jQuery('.mobile-nav .m-nav-container').removeClass('open');
-		});
-		
-		jQuery(document).mouseup(function (e) {
-			var container = jQuery('.mobile-nav .m-nav-container.open');
-			if (!container.is(e.target) && container.has(e.target).length === 0) {
-				jQuery('.mobile-nav .m-nav-container').removeClass('open');
-			}
-		});
-	
-		jQuery(document).on('touchstart', function(e) {
-			var container = jQuery('.mobile-nav .m-nav-container.open');
-			if (!container.is(e.target) && container.has(e.target).length === 0) {
-				jQuery('.mobile-nav .m-nav-container').removeClass('open');
-			}
-		});
 });
