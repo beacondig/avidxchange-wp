aa5c1aa116d882f0729407bf5748df1488230398
Michael McCoy <mmccoy@atmosol.com>
2019-09-26 12:59:58 -0400

    Integrations Responsive and Updates

--- wp-content/themes/bootstrap-genesis-develop/js/integrations-functions.js
+++ wp-content/themes/bootstrap-genesis-develop/js/integrations-functions.js
@@ -1,10 +1,22 @@
 jQuery(function() {
 		// Featured Slider
-		jQuery('ul.featured').bxSlider({
+		jQuery('.slider-container.desktop ul.featured').bxSlider({
 			auto:false,
 			infiniteLoop:true,
-			nextSelector:'.slider-container .next',
-			prevSelector:'.slider-container .prev',
+			nextSelector:'.slider-container.desktop .next',
+			prevSelector:'.slider-container.desktop .prev',
+			speed:1000,
+			pager:false,
+			randomStart:false, 
+			touchEnabled:false
+		});
+		
+		// Featured Slider Mobile
+		jQuery('.slider-container.mobile ul.featured').bxSlider({
+			auto:false,
+			infiniteLoop:true,
+			nextSelector:'.slider-container.mobile .next',
+			prevSelector:'.slider-container.mobile .prev',
 			speed:1000,
 			pager:false,
 			randomStart:false, 
--- wp-content/themes/bootstrap-genesis-develop/template-integrations.php
+++ wp-content/themes/bootstrap-genesis-develop/template-integrations.php
@@ -39,7 +39,7 @@
 					</div>
 				</div>
 				<div class="right-side">
-					<div class="slider-container">
+					<div class="slider-container desktop">
 						<ul class="featured">
 							<li>
 								<div class="featured-logo">
@@ -78,13 +78,41 @@
 						<div class="prev"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/left-arrow-blue.png" /></div>
 						<div class="next"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/right-arrow-blue.png" /></div>
 					</div>
+					<div class="mobile">
+						<div class="search-container">	
+							<div class="search-title">Search</div>
+							<div class="intgmag"><i class="fa fa-search" aria-hidden="true"></i></div>
+							<input type="text" id="intg-search-input" />
+							<div class="alphalist">
+								<?php $intgArray = array(); 
+								$intgLArray = array();
+								$alphas = range('A', 'Z');
+								?>
+								<?php //$loop = new WP_Query( array( 'post_type' => 'integrations', 'posts_per_page' => -1, 'order' => 'ASC', 'orderby' => 'title' ) ); ?>
+								<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+									<?php
+										$intgArray[] = get_the_title();
+										$intgLArray[] = strtoupper(substr(get_the_title(),0,1));
+									?>
+								<?php endwhile; wp_reset_query();   ?>
+								<a data-letter="ALL" class="alphachosen" >ALL</a>
+								<?php foreach($alphas as $item) :
+									if (in_array($item, $intgLArray)) {
+										echo '<a id="allB" data-letter="'.$item.'" >'.$item.'</a> ';
+									} else {
+										echo '<span class="alphapad">'.$item.'</span> ';
+									} 
+								endforeach; wp_reset_postdata(); ?>
+							</div>
+						</div>
+					</div>
 				   <div id="content-sidebar-wrap">
 						<div id="content" class="hfeed">
 							<div class="integrations-wrapper">
 								<?php $ict = 0;
 								$modid = 1;
 								while ( $loop->have_posts() ) : $loop->the_post(); ?>
-									<div class="integration-item threecrunch" data-letter="<?php echo strtoupper(substr(get_the_title(),0,1)); ?>" data-integration="<?php echo get_the_title(); ?>">
+									<div class="integration-item" data-letter="<?php echo strtoupper(substr(get_the_title(),0,1)); ?>" data-integration="<?php echo get_the_title(); ?>">
 										<?php if(get_field('display_as_popup') == "yes"){ ?> 
 											<a class="loopintg" data-toggle="modal" data-target="#pop<?php echo $modid; ?>" style="cursor:pointer;">
 											<?php $modid++; ?>
@@ -126,7 +154,54 @@
 								</div>
 							</div>
 						</div>
+						<div class="slider-container mobile">
+							<div class="prev"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/solutions/solutions-left-arrow.png" /></div>
+							<div class="next"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/solutions/solutions-right-arrow.png" /></div>
+							<ul class="featured">
+								<li>
+									<div class="featured-logo">
+										<div class="banner">Featured</div>
+										<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/solutions/quickbooks-logo-black.png" />
+									</div>
+									<div class="featured-info">
+										<div class="title">Intuit Quickbooks</div>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incidi ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitat ullamco laboris nisi ut aliquip ex. ea commodo conseq. Duis aute irure dolor in rep rehenderit in vo</p>
+										<a class="btn" href="#">Learn More</a>
+									</div>
+								</li>
+								<li>
+									<div class="featured-logo">
+										<div class="banner">Featured</div>
+										<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/solutions/quickbooks-logo-black.png" />
+									</div>
+									<div class="featured-info">
+										<div class="title">Intuit Quickbooks</div>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incidi ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitat ullamco laboris nisi ut aliquip ex. ea commodo conseq. Duis aute irure dolor in rep rehenderit in vo</p>
+										<a class="btn" href="#">Learn More</a>
+									</div>
+								</li>
+								<li>
+									<div class="featured-logo">
+										<div class="banner">Featured</div>
+										<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/solutions/quickbooks-logo-black.png" />
+									</div>
+									<div class="featured-info">
+										<div class="title">Intuit Quickbooks</div>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incidi ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitat ullamco laboris nisi ut aliquip ex. ea commodo conseq. Duis aute irure dolor in rep rehenderit in vo</p>
+										<a class="btn" href="#">Learn More</a>
+									</div>
+								</li>
+							</ul>
+						</div>
 					<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() . '/js/custom/integrations_script.js';?>"></script>
+					<div class="mobile">
+						<div class="a-container">
+							<div class="a-image"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/sidebar-image@2x.png" /></div>
+							<div class="title">Lorem Ipsum</div>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incidi ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitat ullamco</p>
+							<a class="btn" href="#">Learn More</a>
+						</div>
+					</div>
 				</div>
 			</div>
 		</section>
